21a6c7ee7e2f494140567af1065c52587968b15d
NayeemAhmed <nayeem.m.negalur@gmail.com>
2023-10-26 14:40:38 +0530

    chatApp

---
+++ script.js
@@ -0,0 +1,16 @@
+let username = '';
+const btn = document.getElementById('join-chat');
+const usernameInput = document.getElementById('username-input');
+const form = document.getElementById('form');
+const chatroomContainer = document.querySelector('.chatroom-container');
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    username = usernameInput.value;
+    if(username) {
+        form.style.display='none';
+        //make display as block of chatroom-container
+        chatroomContainer.style.display = 'block';
+    }
+
+});
